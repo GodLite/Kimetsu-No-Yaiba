1dc37946c65e87007124973bb8ecd89564c7fbfe
GodLite <fedokavnukov@gmail.com>
2024-08-16 21:20:09 +0400

    new_blood

--- src/main/java/net/asteria/kimetsunoyaiba/item/ModItems.java
+++ src/main/java/net/asteria/kimetsunoyaiba/item/ModItems.java
@@ -1,8 +1,10 @@
 package net.asteria.kimetsunoyaiba.item;
 
 import net.asteria.kimetsunoyaiba.KimetsunoyaibaMod;
+import net.asteria.kimetsunoyaiba.item.custom.Blood;
 import net.asteria.kimetsunoyaiba.itemGroup.ModItemGroups;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Items;
@@ -11,7 +13,7 @@ import net.minecraft.util.Identifier;
 
 public class ModItems {
     public static final Item GROUP_ICON = register(new Item(new Item.Settings()), "group_icon");
-    public static final Item BLOOD = register(new Item(new Item.Settings().food(ModFoodComponents.BLOOD)), "blood", ModItemGroups.MAIN_MOD_GROUP);
+    public static final Item BLOOD = register(new Blood(new Item.Settings()), "blood", ModItemGroups.MAIN_MOD_GROUP);
 
     public static Item register(Item item, String id) {
         return Items.register(new Identifier(KimetsunoyaibaMod.MODID, id), item);
---
+++ src/main/java/net/asteria/kimetsunoyaiba/item/custom/Blood.java
@@ -0,0 +1,53 @@
+package net.asteria.kimetsunoyaiba.item.custom;
+
+import net.minecraft.advancement.criterion.Criteria;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsage;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.stat.Stats;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.UseAction;
+import net.minecraft.world.World;
+
+public class Blood extends Item {
+    private static final int MAX_USE_TIME = 32;
+
+    public Blood(Item.Settings settings) {
+        super(settings);
+    }
+
+    public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
+        if (user instanceof ServerPlayerEntity serverPlayerEntity) {
+            Criteria.CONSUME_ITEM.trigger(serverPlayerEntity, stack);
+            serverPlayerEntity.incrementStat(Stats.USED.getOrCreateStat(this));
+        }
+
+        if (user instanceof PlayerEntity && !((PlayerEntity)user).getAbilities().creativeMode) {
+            stack.decrement(1);
+        }
+
+        if (!world.isClient) {
+            user.addStatusEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 600, 1));
+        }
+
+        return stack;
+    }
+
+    public int getMaxUseTime(ItemStack stack) {
+        return 32;
+    }
+
+    public UseAction getUseAction(ItemStack stack) {
+        return UseAction.DRINK;
+    }
+
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        return ItemUsage.consumeHeldItem(world, user, hand);
+    }
+}
